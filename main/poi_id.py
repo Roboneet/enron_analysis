d8ea2ef2395b5c75e783393189503f86f2408551
Roboneet <neethumariyajoy@gmail.com>
2017-07-10 13:42:05 +0530

    from scratch

--- main/poi_id.py
+++ main/poi_id.py
@@ -6,11 +6,12 @@ sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from class_vis import prettyPicture, output_image
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary','loan_advances'] # You will need to use more features
+features_list = ['poi','salary','loan_advances','director_fees', 'bonus','long_term_incentive', 'total_payments', 'from_this_person_to_poi','from_poi_to_this_person'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -20,6 +21,7 @@ with open("final_project_dataset.pkl", "r") as data_file:
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
+
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
@@ -32,8 +34,8 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -58,21 +60,6 @@ from sklearn.metrics import precision_score
 score = precision_score(labels_test, pred)
 print score
 
-# # plotting data
-
-# import matplotlib.pyplot as plt
-
-# train_color = "r"
-# test_color = "b"
-# for feature, label in zip(features_test, labels_test):
-#     plt.scatter( feature, target, color=test_color ) 
-# for feature, target in zip(features_train, labels_train):
-#     plt.scatter( feature, target, color=train_color )
-
-# plt.xlabel(features_list[1])
-# plt.ylabel(features_list[2])
-# plt.legend()
-# plt.show()
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
